8b80ad36d2f1199deabf19c60f8ebea46f7b0fa3
kisb01 <BudaPest12>
2021-12-06 22:54:43 +0100

    Initialize database, insert not working.

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.model.Category;
+import guru.springframework.recipeapp.model.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+//        Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
+//        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+//
+//        System.out.println("Cat id is: " + optionalCategory.get().getId());
+//        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/recipeapp/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/recipeapp/repositories/RecipeRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.recipeapp.repositories;
 import guru.springframework.recipeapp.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
 }
