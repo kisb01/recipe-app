5f25b686be4de45e4a989e4837e97990405de381
kisb01 <BudaPest12>
2021-12-08 21:04:34 +0100

    Add few test cases. Closes #5.

---
+++ src/test/java/guru/springframework/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.recipeapp.services;
+
+import guru.springframework.recipeapp.model.Recipe;
+import guru.springframework.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
