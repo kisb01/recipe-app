c1f85b3e3689c27c20191e8ff6549fb0a331ee18
kisb01 <BudaPest12>
2021-12-12 19:58:21 +0100

    Add Recipe Controller. Closes #9.

--- src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -25,5 +26,16 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if(recipeOptional.isEmpty()) {
+            throw new RuntimeException("Recipe not Found");
+        }
+
+        return recipeOptional.get();
     }
 }
